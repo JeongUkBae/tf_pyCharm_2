840c44414e23d0f4751a10c98cdd0c3e5b1cc968
gt1854 <gt1854@naver.com>
2019-04-10 18:16:27 +0900

    2019.04.10 18:16

--- seoul_cctv/cctv_pop.py
+++ seoul_cctv/cctv_pop.py
@@ -22,7 +22,7 @@ pop_data_col
 
 df_cctv.rename(columns={df_cctv.columns[0]: '구별'}
                , inplace=True)
-#inplace=True 실제 변수의 내용을 바꿔라.
+# inplace=True 실제 변수의 내용을 바꿔라.
 
 
 df_pop.rename(columns={df_pop.columns[0]: '구별'
@@ -32,29 +32,29 @@ df_pop.rename(columns={df_pop.columns[0]: '구별'
                          , df_pop.columns[4]: '고령자'
                          }, inplace=True)
 
-#print(df_pop.columns)
-#print(df_cctv.columns)
+# print(df_pop.columns)
+# print(df_cctv.columns)
 
-#문제 1 : df_cctv 를 소계 기준 오름차순 정렬
+# 문제 1 : df_cctv 를 소계 기준 오름차순 정렬
 df_cctv.sort_values(by='소계', ascending=True)
 
-#문제 2 : df_pop 에서 0번행 삭제
-#print(df_pop[1:])
+# 문제 2 : df_pop 에서 0번행 삭제
+# print(df_pop[1:])
 df_pop.drop([0],inplace=True) #합계삭제
 
-#문제 3 : df_pop 에서 구별 기준 중복 제거
+# 문제 3 : df_pop 에서 구별 기준 중복 제거
 df_pop['구별'].unique()
 
-#문제 4 : df_pop 에서 null 체크 (null 있는지 여부)
+# 문제 4 : df_pop 에서 null 체크 (null 있는지 여부)
 df_pop[df_pop['구별'].notnull()]
 df_pop.drop([26],inplace=True)
 
-#문제 5 : df_pop 에서 인구수 기준 오름차순 정렬
+# 문제 5 : df_pop 에서 인구수 기준 오름차순 정렬
 df_pop.sort_values(by='인구수', ascending=True)
 
 df_pop['외국인비율'] = (df_pop['외국인'] / df_pop['인구수']) *100
 df_pop['고령자비율'] = (df_pop['고령자'] / df_pop['인구수']) *100
-#문제 6 : df_cctv 에서 '2013년도 이전', '2014년', '2015년', '2016년' 열삭제
+# 문제 6 : df_cctv 에서 '2013년도 이전', '2014년', '2015년', '2016년' 열삭제
             # 결국 df_cctv 는 '구별', '소계' 만 남김
 df_cctv.drop([ '2013년도 이전','2014년','2015년','2016년'],1,inplace=True)
 
@@ -77,4 +77,9 @@ df_cctv_pop.set_index('구별', inplace=True)
 
 cor1 = np.corrcoef(df_cctv_pop['고령자비율'], df_cctv_pop['소계'])
 cor2 = np.corrcoef(df_cctv_pop['외국인비율'], df_cctv_pop['소계'])
-print("고령자비율 상관계수 :: {} \n 외국인비율 상관계수 :: {}".format(cor1,cor2))
+#print("고령자비율 상관계수 :: {} \n 외국인비율 상관계수 :: {}".format(cor1,cor2))
+
+df_cctv_pop.to_csv(ctx+'cctv_pop.csv')
+
+
+
